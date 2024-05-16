81e605c6fe0cb148c61a749b7ccd8374cd52f145
Gabriel Marcello Beitum <149585531+GabrielBeitum@users.noreply.github.com>
2024-05-16 09:23:47 -0300

    Add files via upload

---
+++ src/Componentes/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MinhasRotas from './Estrutura/TodasRotas';
+import './Estrutura/Atividades/Utilitarios/CSS/App.css';
+
+
+function App() {
+  return (
+    <MinhasRotas/>
+  );
+}
+
+export default App;
